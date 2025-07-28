13e60a37620970746318a0b67585a960fb889fc4
hyperfraise <damien.menigaux@polytechnique.edu>
2025-07-28 15:30:56 +0200

    Add the proper neon url

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str = "your-secret-key"  # Replace with a strong secret key
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    DATABASE_URL: str = "postgresql://user:password@localhost/db"
+    DATABASE_URL: str = "postgresql://neondb_owner:npg_mqfC7hVBTw0D@ep-nameless-morning-a2z4vx7f-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
     ADMIN_API_KEY: str = "your-admin-api-key"
 
     class Config:
